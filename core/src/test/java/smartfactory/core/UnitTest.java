b55d569b777308de5941e753e9415db2701de7d8
kinnder <kinnder@mail.ru>
2017-07-04 17:49:16 +0300

    #3 simple test

---
+++ core/src/test/java/smartfactory/core/UnitTest.java
@@ -0,0 +1,14 @@
+package smartfactory.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class UnitTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(4l, 2l + 2l);
+	}
+
+}
