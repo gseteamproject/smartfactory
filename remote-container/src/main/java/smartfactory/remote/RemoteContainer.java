5d743a613d8f62449d324746581d0929a6217d23
kinnder <kinnder@mail.ru>
2017-08-05 14:11:41 +0300

    #7 Remote-container component added

---
+++ remote-container/src/main/java/smartfactory/remote/RemoteContainer.java
@@ -0,0 +1,10 @@
+package smartfactory.remote;
+
+import smartfactory.container.ContainerLauncher;
+
+public class RemoteContainer {
+	
+	public static void main(String[] args) {
+		new ContainerLauncher().start();
+	}
+}
