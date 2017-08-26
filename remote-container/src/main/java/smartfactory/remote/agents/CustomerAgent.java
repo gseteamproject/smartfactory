c7a451ebe0c85ac899264f8156290c2fb6912864
kinnder <kinnder@mail.ru>
2017-08-27 00:34:11 +0300

    #14 remote container startup usecase implemented

---
+++ remote-container/src/main/java/smartfactory/remote/agents/CustomerAgent.java
@@ -0,0 +1,8 @@
+package smartfactory.remote.agents;
+
+import jade.core.Agent;
+
+public class CustomerAgent extends Agent {
+
+	private static final long serialVersionUID = -2432898217068138400L;
+}
