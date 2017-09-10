616c4a188c6a41cddbfffd757273b15fbed1a56f
kinnder <kinnder@mail.ru>
2017-09-10 23:25:29 +0300

    #16 ProductDataStore tests

--- core/src/test/java/smartfactory/dataStores/ProductDataStoreTest.java
+++ core/src/test/java/smartfactory/dataStores/ProductDataStoreTest.java
@@ -1,5 +1,8 @@
 package smartfactory.dataStores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.After;
@@ -7,6 +10,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import smartfactory.dataStores.ProductDataStore;
 import smartfactory.models.Product;
@@ -41,11 +45,38 @@ public class ProductDataStoreTest {
 	}
 
 	@Test
+	public void setProduct() {
+		final Product product_mock = context.mock(Product.class);
+
+		productDataStore.setProduct(product_mock);
+
+		Assert.assertEquals(product_mock, productDataStore.get("product"));
+	}
+
+	@Test
+	public void getRequiredService() {
+		final ServiceDescription service_mock = context.mock(ServiceDescription.class);
+
+		productDataStore.put("requiredService", service_mock);
+
+		Assert.assertEquals(service_mock, productDataStore.getRequiredService());
+	}
+
+	@Test
 	public void setRequiredService() {
 		final ServiceDescription service_mock = context.mock(ServiceDescription.class);
 
 		productDataStore.setRequiredService(service_mock);
 
 		Assert.assertEquals(service_mock, productDataStore.get("requiredService"));
+	}
+
+	@Test
+	public void setAgentsProvidingService() {
+		final List<DFAgentDescription> agentsProvidingService = new ArrayList<DFAgentDescription>();
+
+		productDataStore.setAgentsProvidingService(agentsProvidingService);
+
+		Assert.assertEquals(agentsProvidingService, productDataStore.get("agentsDescription"));
 	}
 }
