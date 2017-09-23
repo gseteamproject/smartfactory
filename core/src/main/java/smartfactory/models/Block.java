5830564497640abf5ff5290c395693ae853459a2
kinnder <kinnder@mail.ru>
2017-09-23 22:44:56 +0300

    #16 Block extends Product

--- core/src/main/java/smartfactory/models/Block.java
+++ core/src/main/java/smartfactory/models/Block.java
@@ -1,10 +1,51 @@
 package smartfactory.models;
 
-public class Block {
+public class Block extends Product {
 
 	public BlockState state;
 
 	public Block() {
 		state = BlockState.initial;
+	}
+
+	@Override
+	public String getRequiredServiceName() {
+		switch (state) {
+		case initial:
+			return "store";
+		default:
+			return super.getRequiredServiceName();
+		}
+	}
+
+	@Override
+	public void moveToNextState() {
+		switch (state) {
+		case initial:
+			state = BlockState.stored;
+			break;
+		case stored:
+			state = BlockState.dirty;
+			break;
+		case dirty:
+			state = BlockState.clean;
+			break;
+		case clean:
+			state = BlockState.painted;
+			break;
+		case painted:
+			state = BlockState.packed;
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public boolean isInTheLastState() {
+		if (state != BlockState.initial) {
+			return true;
+		}
+		return false;
 	}
 }
