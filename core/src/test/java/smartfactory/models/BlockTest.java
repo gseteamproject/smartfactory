0362f922a61b0cc4fda92506bdb5cdba0d380129
kinnder <kinnder@mail.ru>
2017-10-02 23:13:32 +0300

    #16 Block covered with tests

---
+++ core/src/test/java/smartfactory/models/BlockTest.java
@@ -0,0 +1,62 @@
+package smartfactory.models;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockTest {
+
+	Block block;
+
+	@Before
+	public void setUp() {
+		block = new Block();
+	}
+
+	@Test
+	public void moveToNextState() {
+		Assert.assertEquals(BlockState.initial, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.stored, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.dirty, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.clean, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.painted, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.packed, block.state);
+		block.moveToNextState();
+
+		Assert.assertEquals(BlockState.packed, block.state);
+	}
+
+	@Test
+	public void getRequiredServiceName_initial() {
+		block.state = BlockState.initial;
+		Assert.assertEquals("store", block.getRequiredServiceName());
+	}
+
+	@Test
+	public void getRequiredServiceName_stored() {
+		block.state = BlockState.stored;
+		Assert.assertEquals(null, block.getRequiredServiceName());
+	}
+
+	@Test
+	public void isInTheLastState_no() {
+		block.state = BlockState.initial;
+		Assert.assertEquals(Product.IsNotInTheLastState, block.isInTheLastState());
+	}
+
+	@Test
+	public void isInTheLastState_yes() {
+		block.state = BlockState.stored;
+		Assert.assertEquals(Product.IsInTheLastState, block.isInTheLastState());
+	}
+}
