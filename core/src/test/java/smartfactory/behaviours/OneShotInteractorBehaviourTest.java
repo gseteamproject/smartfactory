10edb2c06e677a1612b4018bc4951359b95f2c51
kinnder <kinnder@mail.ru>
2017-10-02 22:07:56 +0300

    #16 tests for OneShotInteractorBehaviour

---
+++ core/src/test/java/smartfactory/behaviours/OneShotInteractorBehaviourTest.java
@@ -0,0 +1,60 @@
+package smartfactory.behaviours;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import smartfactory.interactors.OneShotInteractor;
+
+public class OneShotInteractorBehaviourTest {
+	private final Mockery context = new Mockery() {
+		{
+			this.setImposteriser(ClassImposteriser.INSTANCE);
+		}
+	};
+
+	OneShotInteractor interactor_mock;
+
+	OneShotInteractorBehaviour oneShotInteractorBehaviour;
+
+	@Before
+	public void setUp() {
+		interactor_mock = context.mock(OneShotInteractor.class);
+
+		oneShotInteractorBehaviour = new OneShotInteractorBehaviour(interactor_mock);
+	}
+
+	@After
+	public void tearDown() {
+		context.assertIsSatisfied();
+	}
+
+	@Test
+	public void action() {
+		context.checking(new Expectations() {
+			{
+				oneOf(interactor_mock).execute();
+			}
+		});
+
+		oneShotInteractorBehaviour.action();
+	}
+
+	@Test
+	public void onEnd() {
+		final int transition = 1;
+
+		context.checking(new Expectations() {
+			{
+				oneOf(interactor_mock).next();
+				will(returnValue(transition));
+			}
+		});
+
+		Assert.assertEquals(transition, oneShotInteractorBehaviour.onEnd());
+	}
+}
