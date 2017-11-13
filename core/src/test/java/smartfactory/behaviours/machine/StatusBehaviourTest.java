1cd0d457bdc3c3575645bfa88e6ec70d26944bcf
kkv <kkv@SMART.tps.ifmo.ru>
2017-11-13 16:32:53 +0300

    #30 increased test coverage

---
+++ core/src/test/java/smartfactory/behaviours/machine/StatusBehaviourTest.java
@@ -0,0 +1,53 @@
+package smartfactory.behaviours.machine;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import jade.core.Agent;
+import smartfactory.dataStores.MachineDataStore;
+
+public class StatusBehaviourTest {
+
+	private final Mockery context = new Mockery() {
+		{
+			this.setImposteriser(ClassImposteriser.INSTANCE);
+		}
+	};
+
+	ActivityResponderBehaviour interactionBehaviour_mock;
+
+	MachineDataStore dataStore_mock;
+
+	Agent agent_mock;
+
+	StatusBehaviour testable;
+
+	@Before
+	public void setUp() {
+		interactionBehaviour_mock = context.mock(ActivityResponderBehaviour.class);
+		dataStore_mock = context.mock(MachineDataStore.class);
+		agent_mock = context.mock(Agent.class);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(interactionBehaviour_mock).getAgent();
+				will(returnValue(agent_mock));
+			}
+		});
+
+		testable = new StatusBehaviour(interactionBehaviour_mock, dataStore_mock);
+	}
+
+	@After
+	public void tearDown() {
+		context.assertIsSatisfied();
+	}
+
+	@Test
+	public void constructor() {
+	}
+}
---
+++ core/src/test/java/smartfactory/behaviours/machine/WorkBehaviourTest.java
@@ -0,0 +1,41 @@
+package smartfactory.behaviours.machine;
+
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import smartfactory.dataStores.MachineDataStore;
+
+public class WorkBehaviourTest {
+
+	private final Mockery context = new Mockery() {
+		{
+			this.setImposteriser(ClassImposteriser.INSTANCE);
+		}
+	};
+
+	ActivityResponderBehaviour interactionBehaviour_mock;
+
+	MachineDataStore dataStore_mock;
+
+	WorkBehaviour testable;
+
+	@Before
+	public void setUp() {
+		interactionBehaviour_mock = context.mock(ActivityResponderBehaviour.class);
+		dataStore_mock = context.mock(MachineDataStore.class);
+
+		testable = new WorkBehaviour(interactionBehaviour_mock, dataStore_mock);
+	}
+
+	@After
+	public void tearDown() {
+		context.assertIsSatisfied();
+	}
+
+	@Test
+	public void constructor() {
+	}
+}
