d2ce450198a0d6047a06aff30224c343cfcf2a8e
kinnder <kinnder@mail.ru>
2017-11-21 23:56:43 +0300

    #29 added OrderBehaviour dummy

--- core/src/main/java/smartfactory/agents/OrderAgent.java
+++ core/src/main/java/smartfactory/agents/OrderAgent.java
@@ -1,10 +1,29 @@
 package smartfactory.agents;
 
+import smartfactory.behaviours.order.OrderBehaviour;
+import smartfactory.models.Order;
+
 public class OrderAgent extends SmartFactoryAgent {
 
 	private static final long serialVersionUID = -5529204273917293075L;
 
 	public static String getUniqueName() {
 		return "order-" + Long.toString(System.currentTimeMillis());
+	}
+
+	private Order order;
+
+	public Order createOrder() {
+		return new Order();
+	}
+
+	@Override
+	protected void initializeBehaviours() {
+		addBehaviour(new OrderBehaviour());
+	}
+
+	@Override
+	protected void initializeData() {
+		order = createOrder();
 	}
 }
---
+++ core/src/main/java/smartfactory/behaviours/order/OrderBehaviour.java
@@ -0,0 +1,8 @@
+package smartfactory.behaviours.order;
+
+import jade.core.behaviours.FSMBehaviour;
+
+public class OrderBehaviour extends FSMBehaviour {
+
+	private static final long serialVersionUID = -7362885572667233532L;
+}
