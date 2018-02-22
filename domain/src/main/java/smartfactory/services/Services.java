39c5c2d7cc2d6e9e6de88b312774eed70622f1a5
kinnder <kinnder@mail.ru>
2018-02-22 15:42:29 +0300

    #31 lego robot specific services added

--- domain/src/main/java/smartfactory/services/Services.java
+++ domain/src/main/java/smartfactory/services/Services.java
@@ -6,4 +6,7 @@ public class Services {
 	public final static String cleaning = "cleaning";
 	public final static String painting = "painting";
 	public final static String packing = "packing";
+	public final static String cleaning_worker = "cleaning-worker";
+	public final static String painting_worker = "painting-worker";
+	public final static String transport = "transport";
 }
