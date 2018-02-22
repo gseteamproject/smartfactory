0c419fa54da1aafb8dbdf93424a1b5815f8468ae
kinnder <kinnder@mail.ru>
2018-02-22 14:41:32 +0300

    #34 use-case implemented

--- factory-container/src/main/java/smartfactory/models/PaintingStation.java
+++ factory-container/src/main/java/smartfactory/models/PaintingStation.java
@@ -1,4 +1,11 @@
 package smartfactory.models;
 
+import smartfactory.services.Services;
+
 public class PaintingStation extends Resource {
+
+	public PaintingStation() {
+		super();
+		addOperation(new ResourceOperation(Services.painting));
+	}
 }
--- lego-container/src/main/java/smartfactory/models/Lego.java
+++ lego-container/src/main/java/smartfactory/models/Lego.java
@@ -1,11 +1,8 @@
 package smartfactory.models;
 
-import smartfactory.services.Services;
-
 public class Lego extends Resource {
 
 	public Lego() {
 		super();
-		addOperation(new ResourceOperation(Services.painting));
 	}
 }
