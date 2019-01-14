9f8d64a7bb6629185d22288a39eb4e16cafcff0f
kinnder <kinnder@mail.ru>
2019-01-14 15:05:28 +0300

    unittests for Interactor class

--- core/src/test/java/smartfactory/interactors/InteractorTest.java
+++ core/src/test/java/smartfactory/interactors/InteractorTest.java
@@ -1,12 +1,17 @@
 package smartfactory.interactors;
 
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import jade.content.ContentElement;
+import jade.lang.acl.ACLMessage;
 import smartfactory.interactors.Interactor;
+import smartfactory.platform.AgentPlatform;
 import smartfactory.utility.AgentDataStore;
 
 public class InteractorTest {
@@ -34,6 +39,39 @@ public class InteractorTest {
 	}
 
 	@Test
-	public void constructor() {
+	public void extractContent() {
+		final ACLMessage message_mock = context.mock(ACLMessage.class);
+		final ContentElement contentElement_mock = context.mock(ContentElement.class);
+		final AgentPlatform agentPlatform_mock = context.mock(AgentPlatform.class);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(dataStore_mock).getAgentPlatform();
+				will(returnValue(agentPlatform_mock));
+
+				oneOf(agentPlatform_mock).extractContent(message_mock);
+				will(returnValue(contentElement_mock));
+			}
+		});
+
+		Assert.assertEquals(contentElement_mock, testable.extractContent(message_mock));
+	}
+
+	@Test
+	public void fillContent() {
+		final ACLMessage message_mock = context.mock(ACLMessage.class);
+		final ContentElement contentElement_mock = context.mock(ContentElement.class);
+		final AgentPlatform agentPlatform_mock = context.mock(AgentPlatform.class);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(dataStore_mock).getAgentPlatform();
+				will(returnValue(agentPlatform_mock));
+
+				oneOf(agentPlatform_mock).fillContent(message_mock, contentElement_mock);
+			}
+		});
+
+		testable.fillContent(message_mock, contentElement_mock);
 	}
 }
